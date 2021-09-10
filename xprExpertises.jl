dd6a2c3687cfb534cb1be3e040bec2bed6a21eab
sardinecan <morvan.josselin@gmail.com>
2021-09-10 16:21:04 +0200

    reviewed network metrics

--- xprExpertises.jl
+++ xprExpertises.jl
@@ -10,51 +10,81 @@ using Colors
 using Statistics
 using UnicodePlots
 
-#crée un dataframe à partir des données tabulaires de l'année.
-data = CSV.File(HTTP.get("https://experts.huma-num.fr/xpr/networks/1726/expertises").body, header=1) |> DataFrame
-data1 = CSV.File(HTTP.get("https://experts.huma-num.fr/xpr/networks/1726/categories").body, header=1) |> DataFrame
+#########################################################
+# Données tabulaires brutes
+#########################################################
+# Année étudiée
+year = 1726
+# dataframe experts par expertises
+expertisesNetwork = CSV.File(HTTP.get("https://experts.huma-num.fr/xpr/networks/$year/expertises").body, header=1) |> DataFrame
+
+# dataframe experts par categories
+categoriesNetwork = CSV.File(HTTP.get("https://experts.huma-num.fr/xpr/networks/$year/categories").body, header=1) |> DataFrame
+
+# données sur les experts
+expertsData = CSV.File(HTTP.get("https://experts.huma-num.fr/xpr/data/$year/experts").body, header=1) |> DataFrame
+
+# données sur les expertises
+expertisesData = CSV.File(HTTP.get("https://experts.huma-num.fr/xpr/data/$year/expertises").body, header=1) |> DataFrame
 
 # @todo mettre en évidence la répartition inégale des affaires entre experts
 
-#création d'un vecteur contenant les noms des experts à partir de la colone label du dataframe
-expertisesNodes = data[!, :label]
-#nb d'experts
-numExpertises = length(expertisesNodes)
-#création d'un vecteur contenant les noms des catégories d'expertise à partir du nom des colonnes du dataframe
-expertNames = data1[!, :label]
-expertNodes = names(select(data, Not(:label)))
-#nb de catégories
-numExperts = length(expertNodes)
+# Les données regroupe 468 d'affaires dépouillées qui représentent un nombre moyen d'affaires dans le dépouillement pour le début de la période étudiée.
+# Nous avons 40 experts qui sont tirés du dépouillement des almanachs (année N + année N+1, les alamanchs étant préparé en aout de l'année précédente) et croisé avec les données prosopographiques trouvée par juliette, ainsi que les experts nommés dans les affaires. (1 pour 1726 Montbrouard)
+# Une affaire peut
+sum(eachcol(expertisesNetwork[:, Not(:id)]))
+#########################################################
+# création du métagraphe
+#########################################################
+
+# vecteur contenant les id des experts
+experts = expertsData[!, :id]
+# vecteur contenant les noms des experts
+expertNames = expertsData[!, :surname]
+# vecteur contenant la catégorie des experts
+expertsColumns = expertsData[!, :column]
+# nb d'experts
+numExperts = length(experts)
+
+# vecteur contenant les id expertises
+expertises = expertisesData[!, :id]
+# nb d'expertises
+numExpertises = length(expertises)
+
 #nb total de nœuds (experts + categories)
 numNodes = numExpertises + numExperts
-#Création du Graph
+
+# création du Graph
 expertisesGraph = MetaGraph(SimpleGraph())
-#création du nombre de nœuds total, ne nécessite pas de faire de boucle, ajoute autant de nœuds que le nb indiqué en arg2
+
+# Création des nœuds
 add_vertices!(expertisesGraph, numNodes)
-# pour les n premiers nœuds correspondants aux experts => création des métadonnées liées
-for expertise in 1:numExpertises
-    set_prop!(expertisesGraph, expertise, :label, expertisesNodes[expertise])
-    set_prop!(expertisesGraph, expertise, :cat, "expertise")
-end
-expertisesGraph.vprops
-pos = 1
-for expert in (numExpertises+1):numNodes
-    for i in 1:numExperts
-        set_prop!(expertisesGraph, expert, :label, expertNodes[pos])
-    end
+
+# ajout des métadonnées pour les nœuds experts
+for expert in 1:numExperts
+    set_prop!(expertisesGraph, expert, :id, experts[expert])
+    set_prop!(expertisesGraph, expert, :name, expertNames[expert])
+    set_prop!(expertisesGraph, expert, :column, expertsColumns[expert])
     set_prop!(expertisesGraph, expert, :cat, "Expert")
+end
+
+# ajout des métadonnées pour les nœuds expertises
+pos = 1
+for expertise in (numExperts+1):numNodes
+    for i in 1:numExpertises
+        set_prop!(expertisesGraph, expertise, :id, expertises[pos])
+        set_prop!(expertisesGraph, expertise, :cat, "expertise")
+    end
     global pos += 1
 end
 
-expertisesGraph.vprops[500]
-
+# ajout des edges
 col = 1
-for column in expertNodes
-    print(col)
+for column in expertises
     pos = 1
-    for expertise in data[!, column]
-        if data[!, column][pos] > 0
-            add_edge!(expertisesGraph, pos, col+numExpertises)
+    for expert in expertisesNetwork[!, column]
+        if expertisesNetwork[!, column][pos] > 0
+            add_edge!(expertisesGraph, pos, col+numExperts)
         end
         global pos += 1
     end
@@ -62,52 +92,179 @@ for column in expertNodes
 end
 #control
 collect(edges(expertisesGraph))
-#création d'un vecteur pour la couleur
+
+#########################################################
+# VIZ
+#########################################################
 nodecolor = Vector()
 #pour chaque nœuds du graph, on vérifie son type pour mettre à jour le vecteur (valeur 1 ou 2 correspondant au position du vecteur color ci-après)
 for i in sort(collect(keys(expertisesGraph.vprops)))
-    if expertisesGraph.vprops[i][:cat] == "Expert"
-        push!(nodecolor, 1)
+  if expertisesGraph.vprops[i][:cat] == "Expert"
+    if expertisesGraph.vprops[i][:column] == "architecte"
+      push!(nodecolor, 1)
+    elseif expertisesGraph.vprops[i][:column] == "entrepreneur"
+      push!(nodecolor, 2)
     else
-        push!(nodecolor, 2)
+      push!(nodecolor, 3)
     end
+  else
+    push!(nodecolor, 4)
+  end
 end
-#vecteur couleur
-color = [colorant"lightseagreen", colorant"orange"]
-#creation du vecteur couleur (rgb)
+# vecteur couleurs
+color = [colorant"#FF4500", colorant"#19FFD1", colorant"#FFF819", colorant"#700DFF"]
+ # creation du vecteur expert/couleur(rgb)
 nodefillc=color[nodecolor]
-layout=(args...)->spring_layout(args...; C=20)
+layout=(args...)->spring_layout(args...; C=30)
 gplot(expertisesGraph, nodefillc=nodefillc, layout=layout)
-ExpertiseMatrix = Matrix(adjacency_matrix(expertisesGraph))
-m = ExpertiseMatrix[1:numExpertises, (numExpertises+1):numNodes]
-mp = transpose(m) * m
-mt = m * transpose(m)
-gmp = Graph(mp)
-nodesizeGmp = [LightGraphs.outdegree(Graph(gmp), v) for v in 1:numExperts]
 
-gplot(gmp, nodelabel=expertNames, layout=layout, nodesize=nodesizeGmp)
+#########################################################
+# Conversion vers des données unimodales
+#########################################################
+# Matrice bimodale experts par expertises
+expertisesMatrix = Matrix(adjacency_matrix(expertisesGraph))
+m = expertisesMatrix[1:numExperts, (numExperts+1):numNodes]
 
 # certains experts participent à plus d’affaires, Borgatti suggère de normaliser les valeurs en utilisant Bonacich
 
 # dans m, diviser les valeurs de la matrice d’origine par la √ de la somme de la colonne d’origine
-
-colsum = sum(m, dims=1)
+colsum = sum(m, dims=2)
 
 ponderation = vec(broadcast(√, colsum))
 
 ponderate(x) = x ./ ponderation
 
 # calculer la matrice normalisée en supprimant les valeurs NaN
-normm = replace!(mapslices(x -> ponderate(x), m, dims=2), NaN => 0)
+normm = replace!(mapslices(x -> ponderate(x), m, dims=1), NaN => 0)
 
 # matrice normalisée projetée
 normmp = transpose(normm) * normm
 
 # matrice normalisée transposée
+# Cette valeur est interprétée comme un indice de la force de la proximité sociale entre deux experts.
+# Pour l'interprétation, voir Bogartti 1997 p. 245-246
 normmt = normm * transpose(normm)
 
+#graphe de la matrice normalisée transposée
+gnormmt = MetaGraph(SimpleGraph(normmt))
 
-gplot(Graph(normmp), layout=layout)
 
-nodesize = vec(sum(normmp, dims=1))
-gplot(Graph(normmp), layout=layout, nodesize=nodesize, )
+#########################################################
+# VIZ
+#########################################################
+
+# pondération des edges avec les valeurs normalisées
+edgesCollection = collect(edges(gnormmt))
+
+edgelinewidth = Vector()
+for i in edgesCollection
+    edgesString = split(string(i), " ")
+    from = parse(Int64, edgesString[2])
+    to = parse(Int64, edgesString[4])
+    push!(edgelinewidth, normmt[from, to])
+end
+# pondération de la taille de nœuds avec la sommes des valeurs normalisées pour chaque experts
+nodesize = vec(sum(normmt, dims=1))
+
+# attribution d'un couleur pour les experts
+expertColor = nodecolor[1:numExperts]
+expertfillc=color[expertColor]
+
+# couleur des labels experts
+nodelabelc = colorant"white"
+
+# paramètres du layout
+expertLayout=(args...)->spring_layout(args...; C=20)
+
+
+gplot(gnormmt, nodelabel=expertNames, nodelabelc=nodelabelc, nodelabeldist=3.5, nodelabelangleoffset=π/2, nodesize=nodesize, nodefillc=expertfillc, layout=expertLayout, edgelinewidth=edgelinewidth)
+
+#########################################################
+# Metrics
+#########################################################
+
+# Bigraph experts par expertises (expertisesGraph)
+
+# La densité d'un réseau bipartite nb edges/(nb experts * nb affaires) puisque les nœuds d'un mode ne peuvent pas avoir de relations entre eux.
+# si densité = 1 tous les nœuds sont connectés entre eux, si 0 aucune connection.
+bigraphDensity = ne(expertisesGraph)/(numExperts*numExpertises)
+
+# Centralité de degré
+# C'est la somme des edges d'un nœud
+# dans notre graphe bimodal, pour un expert c'est le nombre d'expertises auxquelles il participe, et pour une expertise, c'est le nombre d'experts qui y participe
+bigraphDeg = vec(sum(expertisesMatrix, dims=2))
+
+# Il est possible, pour interpréter cette valeur de la normaliser
+# Pour Borgatti, la méthode Freeman n'est pas adaptée pour les données bimodales (Borgatti 1997, p. 254)
+# On divise le degré de chaque nœud par le nombre de nœuds de l'autre mode
+bigraphNormDeg = Vector()
+for i in 1:numNodes
+    if i <= numExperts
+        value = (bigraphDeg[i]/numExpertises)*100
+        push!(bigraphNormDeg, value)
+    else
+        value = (bigraphDeg[i]/numExperts)*100
+        push!(bigraphNormDeg, value)
+    end
+end
+
+# centralité de proximité
+# Indique à quel point un nœud est proche de tous les autres nœuds du réseau.
+# Elle est calculée comme la moyenne de la longueur du chemin le plus court entre le nœud et tous les autres nœuds du réseau.
+# ex le réseau A-B-C-D-E-F (lettres = nœuds et tirets les edges)
+# pour D, sa distance la plus courte à A = 3 (nombre de edges), B=2, C=1, E=1, F=2
+# la centralité de proximité pour D = distance la plus courte à chaque nœuds (3+2+1+1+2)/(nb nœuds-1) dans un graphe normal
+# plus la valeur est faible plus le nœud est central
+
+# normalisation du bigraph (normalisation faite plus haut)
+bigraphNorm = normm
+
+#degree(gnormmp)
+betweennessCentrality = betweenness_centrality(gnormmt)
+closenessCentrality = closeness_centrality(gnormmt)
+degreeCentrality = degree_centrality(gnormmt)
+eigenvectorCentrality = eigenvector_centrality(gnormmt)
+katzCentrality = katz_centrality(gnormmt)
+#pagerank = pagerank(gnormmp)
+radialityCentrality = radiality_centrality(gnormmt)
+stressCentrality = stress_centrality(gnormmt)
+
+
+
+bigraphDegreeCentrality = rowsum[1:numExperts]/numExpertises
+
+metrics = DataFrame(expert = expertNames, colonne=expertsColumns, normDeg=bigraphNormDeg[1:40],  bigraphDegreeCentrality=bigraphDegreeCentrality, degree=degree(gnormmt), betweennessCentrality=betweenness_centrality(gnormmt), closenessCentrality=closenessCentrality, degreeCentrality=degreeCentrality, eigenvectorCentrality=eigenvectorCentrality, katzCentrality=katzCentrality, pagerank=pagerank(gnormmt), radialityCentrality=radialityCentrality, stressCentrality=stressCentrality)
+metricsArchi = metrics[metrics[!, :colonne] .== "architecte", :]
+
+# community detection
+label_propagation(gnormmt)
+maximal_cliques(gnormmt)
+
+
+nlist = Vector{Vector{Int}}(undef, 2) # two shells
+nlist[1] = 1:46 # first shell
+nlist[2] = 47:514 # second shell
+
+locs_x, locs_y = shell_layout(expertisesGraph, nlist)
+
+locs_x = Vector()
+for i in 1:numNodes
+    if i < 47
+        push!(locs_x, 1.00)
+    else
+        push!(locs_x, 2.00)
+    end
+end
+
+locs_y = Vector()
+for i in 1:numNodes
+    if i < 47
+        push!(locs_y, Float64(i))
+    else i > 46
+        push!(locs_y, Float64(i-46))
+    end
+end
+locs_y
+gplot(expertisesGraph, locs_x, locs_y)
+
+readedgelist(expertisesGraph)
